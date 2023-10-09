e590fc54afd1631acc51ae71cc3899914ba14c9a
shyamchaganti <98919556+shyamchaganti@users.noreply.github.com>
2023-10-09 13:31:23 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ module "ec2_instance" {
   ami                    = "ami-0924fa21f01c23f6e"
   instance_type          = "t2.micro"
   monitoring             = true
-  vpc_security_group_ids = ["sg-0601fb5aae1d0194b"]
-  subnet_id              = "subnet-03dcaa293d3dfc5a4"
+  vpc_security_group_ids = ["sg-0788fc1301e46ce04"]
+  subnet_id              = "subnet-064d8d3141a71dfc0"
 
   tags = {
     Terraform   = "true"
